d338ebfb4ca8a90889787ae85921c1996a28f2c2
wzwen3 <2550428014@qq.com>
2020-09-29 09:36:53 +0800

    test1

---
+++ lib/src/page/home.dart
@@ -0,0 +1,17 @@
+/*
+ * @author:  lance
+ * @email:   wzwen3@grgbanking.com
+ * @time:    2020/9/29 8:18
+ * @version: 1.0
+ * @desc:
+ */
+import 'package:flutter/cupertino.dart';
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('1111111'),
+    );
+  }
+}
